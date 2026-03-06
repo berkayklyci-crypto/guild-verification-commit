ed0ff8550646a4377e60f37bdd3a4f50a8fb4986
berkayklyci-crypto <berkayklyci@gmail.com>
2026-03-06 17:30:41 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:30:43.319Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T14:30:44.989Z
+Commit: Resolve null pointer exception in user service
