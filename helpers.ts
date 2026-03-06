fb502ce3227fe2b7bfae54e14b8e9b5ca0d28ee7
berkayklyci-crypto <berkayklyci@gmail.com>
2026-03-06 17:29:57 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:29:50.025Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T14:30:01.192Z
+Commit: Patch API endpoint error handling
