21123c20ffa6d63dbde6f97a26dede2ad776b3a3
berkayklyci-crypto <berkayklyci@gmail.com>
2026-03-06 17:28:57 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:28:46.629Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T14:29:01.635Z
+Commit: Resolve null pointer exception in user service
