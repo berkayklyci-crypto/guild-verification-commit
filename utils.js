2f75637fad7fc3dee35c7a105b2ec5de708145d7
berkayklyci-crypto <berkayklyci@gmail.com>
2026-03-06 17:28:49 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:28:48.863Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T14:28:53.448Z
+Commit: Fix race condition in async operations
