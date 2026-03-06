8f4cc527fb463f8a22a60f786be407549fcc4999
berkayklyci-crypto <berkayklyci@gmail.com>
2026-03-06 17:30:45 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:30:47.660Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T14:30:49.465Z
+Commit: Fix memory leak in event handlers
